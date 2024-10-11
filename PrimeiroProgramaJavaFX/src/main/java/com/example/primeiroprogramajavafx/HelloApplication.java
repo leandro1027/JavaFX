1cb5d471269b19aa07b2e72cfe10596fecade830
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2024-10-11 20:46:29 -0300

    .

--- PrimeiroProgramaJavaFX/src/main/java/com/example/primeiroprogramajavafx/HelloApplication.java
+++ PrimeiroProgramaJavaFX/src/main/java/com/example/primeiroprogramajavafx/HelloApplication.java
@@ -3,6 +3,10 @@ package com.example.primeiroprogramajavafx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +14,21 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        Label lblNome= new Label("Informe seu nome");
+
+        TextField textNome= new TextField();
+        textNome.setPrefSize(100, 50);
+
+        Button btnOK= new Button();
+        btnOK.setText("OK");
+
+        Label lblMensagem= new Label();
+        VBox layout= new VBox();
+        layout.getChildren().addAll(lblNome,textNome,btnOK, lblMensagem);
+
+
+        Scene scene = new Scene(layout, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
